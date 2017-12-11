f288d9aa5bfd6faaf4ae89be7b277280f5f3c9ba
Somo <somo@mombo.solutions>
2017-12-11 02:54:55 -0600

    implement general dnn multi layer model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #%% Imports
+from functools import reduce
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -28,14 +29,12 @@ EXAMPLES_count = data['train'].images.shape[0] # 55000 images (train), 5000 imag
 BATCH_SIZE = EXAMPLES_count
 
 LAMBDA = 0
-HIDDEN_UNITS = 1024
-EPOCHS = 0
+HIDDEN_UNITS = [1024]
+EPOCHS = 10
 ALPHA = 0.01
 DISPLAY_EVERY = 1
 
 
-
-#%%
 X = tf.placeholder(tf.float32, [None, FEATURES_count])
 Y = tf.placeholder(tf.float32, [None, CLASSES_count])
 
@@ -47,24 +46,29 @@ randInit = tf.contrib.layers.xavier_initializer(uniform=False, seed=None)
 softmax = lambda z: tf.nn.softmax(z)
 relu = lambda z: tf.nn.relu(z)
 
-Theta1 = tf.get_variable("Theta1", [FEATURES_count, HIDDEN_UNITS], initializer=randInit)
-biases1 = tf.get_variable("biases1", [HIDDEN_UNITS], initializer=randInit)
 
-Theta2 = tf.get_variable("Theta2", [HIDDEN_UNITS, CLASSES_count], initializer=randInit)
-biases2 = tf.get_variable("biases2", [CLASSES_count], initializer=randInit)
+LAYERS_SHAPE = [FEATURES_count, *HIDDEN_UNITS, CLASSES_count]
 
-Theta = tf.transpose(tf.matmul(Theta1, Theta2))
+# Theta = [tf.get_variable("Theta1", [FEATURES_count, HIDDEN_UNITS[0]], initializer=randInit)]
+# biases = [tf.get_variable("biases1", [HIDDEN_UNITS[0]], initializer=randInit)]
 
-Z1 = tf.add(tf.matmul(X, Theta1), biases1)
-A = softmax(Z1) # Todo: try relu here
+Theta = []
+biases = [] 
+Z = [] 
+A = [X]
 
-Z2 = tf.add(tf.matmul(A, Theta2), biases2)
-H = softmax(Z2)
+for i in range(len(LAYERS_SHAPE)-1) :
+    Theta.append( tf.get_variable("Theta{}".format(i), [LAYERS_SHAPE[i], LAYERS_SHAPE[i+1]], initializer=randInit) )
+    biases.append( tf.get_variable("biases{}".format(i), [LAYERS_SHAPE[i+1]], initializer=randInit) )
 
-# cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(H), reduction_indices=[1]))
-cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=Z2))
 
-accuracy = 100*tf.reduce_mean(tf.cast(tf.equal(tf.argmax(Y, 1), tf.argmax(Z2, 1)), tf.float32))
+    Z.append( tf.add(tf.matmul(A[i], Theta[i]), biases[i]) )
+    A.append( softmax(Z[i]) )
+
+
+cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=Z[-1]))
+
+accuracy = 100*tf.reduce_mean(tf.cast(tf.equal(tf.argmax(Y, 1), tf.argmax(Z[-1], 1)), tf.float32))
 
 loss = cost
 regularization = tf.constant(0)
@@ -73,22 +77,19 @@ regularization = tf.constant(0)
 if (LAMBDA > 0 and BATCH_SIZE >= 1):
     l2 =  lambda t: float(LAMBDA/BATCH_SIZE)*tf.nn.l2_loss(t)
 
-    regularization = tf.add(l2(Theta1), l2(Theta2)) 
+    regularization = reduce(lambda Theta1, Theta2: tf.add(l2(Theta1), l2(Theta2)), Theta)
     loss =  tf.add(cost, regularization) 
 
 # train_step = tf.train.AdamOptimizer(ALPHA).minimize(loss)
 train_step = tf.train.GradientDescentOptimizer(ALPHA).minimize(loss)
 
-
 init = tf.global_variables_initializer()
 
-saver = tf.train.Saver({
-    "Theta1": Theta1,
-    "biases1": biases1,
-    "Theta2": Theta2,
-    "biases2": biases2,
-})
 
+saver = tf.train.Saver({
+    **{'Theta'+str(k):v for k, v in enumerate(Theta)},    
+    **{'biases'+str(k):v for k, v in enumerate(biases)}
+})
 
 with tf.Session() as sess:
 
@@ -96,7 +97,7 @@ with tf.Session() as sess:
     sess.run(init)
 
     try:
-        saver.restore(sess, "./saved_params/model.ckpt")
+        saver.restore(sess, "./saved/params.ckpt")
     except: 
         pass    
 
@@ -132,9 +133,11 @@ with tf.Session() as sess:
     report(data['test'], 'TEST ')
 
     if(EPOCHS>0): 
-        save_path = saver.save(sess, "./saved_params/model.ckpt")
+        save_path = saver.save(sess, "./saved/params.ckpt")
         print("\n***Model saved in file: {}".format(save_path))
 
-    disp(sess.run(Theta))
+    
+    Theta_Product = reduce(lambda Theta1, Theta2: tf.matmul(Theta1, Theta2), Theta)
+    disp(sess.run(tf.transpose(Theta_Product)))
 
     # disp(sess.run(A, feed_dict={X: mnist.test.images[0:16,:], Y: mnist.test.labels[0:16,:]}) )
